2592aaba24155cb1ed2492cd8c8ef1236e495551
@up202409828 <pedromeruge@gmail.com>
2024-10-13 16:19:31 +0100

    Fixed empty entries in merged_games dataset

--- datasets/main.py
+++ datasets/main.py
@@ -8,8 +8,8 @@ def main():
         snd_ds_output_path = 'giantbomb_related/scraped_info.json'
         merged_ds_output_path = 'merged_games.json'
 
-        # first_DS.process_dataset(first_ds_output_path)
-        # giantbomb_DS.scrape(first_ds_output_path, snd_ds_output_path)
+        first_DS.process_dataset(first_ds_output_path)
+        giantbomb_DS.scrape(first_ds_output_path, snd_ds_output_path)
         Common.merge_datasets(first_ds_output_path, snd_ds_output_path, "name", merged_ds_output_path)
     except Exception as e:
         print(e)
