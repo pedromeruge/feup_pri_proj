3ac2564381860d10b47c747bc4ebdab67b65bdb8
@up202409828 <pedromeruge@gmail.com>
2024-10-17 13:28:09 +0100

    Updated conceptual model

--- datasets/main.py
+++ datasets/main.py
@@ -12,18 +12,18 @@ def main():
     try:
         first_ds_output_path = 'first_dataset/parsed_games.json'
         temp_snd_ds_output_path = 'giantbomb_related/scraped_info.json'
-        final_snd_ds_output_path = 'giantbomb_related/parsed_scraped_info.json'
+        final_snd_ds_output_path = 'giantbomb_related/parsed_giantbomb_info.json'
         giantbomb_merged_ds_output_path = 'giantbomb_related/merged_games_1.json'
         ign_reviews_path = 'ign_reviews/parsed_ign_reviews.csv'
         temp_merged_ds_output_path = 'ign_reviews/merged_games_2.json'
         final_merged_ds_output_path = 'merged_games_final.json'
 
         first_DS.process_dataset(first_ds_output_path)
-        # giantbomb_DS.scrape(first_ds_output_path, snd_ds_output_path)
+        # giantbomb_DS.scrape(first_ds_output_path, temp_snd_ds_output_path)
         # giantbomb_process.process_dataset(temp_snd_ds_output_path,final_snd_ds_output_path)
         giantbomb_merge.merge_initial_dataset_with_giantbomb(first_ds_output_path, final_snd_ds_output_path, "name", giantbomb_merged_ds_output_path)
         
-        ## add the ign scraping steps here
+        ## add the ign scraping function calls here
         ## ...
         ign_merge.merge_curr_dataset_with_ign_review(giantbomb_merged_ds_output_path, ign_reviews_path, "name", temp_merged_ds_output_path)
 
